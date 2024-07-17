47c52a1bfa6c4d26f50ca0a7700ebb952662f73a
HP <HP@VENOMIZER>
2024-07-17 18:11:55 +0300

    Add project files.

---
+++ LIBRARYMANAGEMENTSYSTEMS/Form1.Designer.vb
@@ -0,0 +1,269 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.dvgBooks = New System.Windows.Forms.DataGridView()
+        Me.LibraryDBDataSet = New LIBRARYMANAGEMENTSYSTEMS.LibraryDBDataSet()
+        Me.BookssBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BookssTableAdapter = New LIBRARYMANAGEMENTSYSTEMS.LibraryDBDataSetTableAdapters.BookssTableAdapter()
+        Me.IDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TitleDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.AuthorDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.YearPublishedDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.GenreDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.dvgBooks, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LibraryDBDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BookssBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(30, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Title"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(30, 127)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Author"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(30, 208)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(153, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "YearPublished"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(30, 297)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(71, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Genre"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(184, 35)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 31)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(184, 127)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 31)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(184, 208)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 31)
+        Me.TextBox3.TabIndex = 6
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(184, 297)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 31)
+        Me.TextBox4.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(137, 358)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(147, 41)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "btnAdd"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(137, 426)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(147, 41)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "btnUpdate"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(403, 358)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(147, 41)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "btnDelete"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(403, 426)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(147, 41)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "btnClear"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'dvgBooks
+        '
+        Me.dvgBooks.AutoGenerateColumns = False
+        Me.dvgBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dvgBooks.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IDDataGridViewTextBoxColumn, Me.TitleDataGridViewTextBoxColumn, Me.AuthorDataGridViewTextBoxColumn, Me.YearPublishedDataGridViewTextBoxColumn, Me.GenreDataGridViewTextBoxColumn})
+        Me.dvgBooks.DataSource = Me.BookssBindingSource
+        Me.dvgBooks.Location = New System.Drawing.Point(444, 56)
+        Me.dvgBooks.Name = "dvgBooks"
+        Me.dvgBooks.RowHeadersWidth = 82
+        Me.dvgBooks.RowTemplate.Height = 33
+        Me.dvgBooks.Size = New System.Drawing.Size(295, 265)
+        Me.dvgBooks.TabIndex = 13
+        '
+        'LibraryDBDataSet
+        '
+        Me.LibraryDBDataSet.DataSetName = "LibraryDBDataSet"
+        Me.LibraryDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'BookssBindingSource
+        '
+        Me.BookssBindingSource.DataMember = "Bookss"
+        Me.BookssBindingSource.DataSource = Me.LibraryDBDataSet
+        '
+        'BookssTableAdapter
+        '
+        Me.BookssTableAdapter.ClearBeforeFill = True
+        '
+        'IDDataGridViewTextBoxColumn
+        '
+        Me.IDDataGridViewTextBoxColumn.DataPropertyName = "ID"
+        Me.IDDataGridViewTextBoxColumn.HeaderText = "ID"
+        Me.IDDataGridViewTextBoxColumn.MinimumWidth = 10
+        Me.IDDataGridViewTextBoxColumn.Name = "IDDataGridViewTextBoxColumn"
+        Me.IDDataGridViewTextBoxColumn.ReadOnly = True
+        Me.IDDataGridViewTextBoxColumn.Width = 200
+        '
+        'TitleDataGridViewTextBoxColumn
+        '
+        Me.TitleDataGridViewTextBoxColumn.DataPropertyName = "Title"
+        Me.TitleDataGridViewTextBoxColumn.HeaderText = "Title"
+        Me.TitleDataGridViewTextBoxColumn.MinimumWidth = 10
+        Me.TitleDataGridViewTextBoxColumn.Name = "TitleDataGridViewTextBoxColumn"
+        Me.TitleDataGridViewTextBoxColumn.Width = 200
+        '
+        'AuthorDataGridViewTextBoxColumn
+        '
+        Me.AuthorDataGridViewTextBoxColumn.DataPropertyName = "Author"
+        Me.AuthorDataGridViewTextBoxColumn.HeaderText = "Author"
+        Me.AuthorDataGridViewTextBoxColumn.MinimumWidth = 10
+        Me.AuthorDataGridViewTextBoxColumn.Name = "AuthorDataGridViewTextBoxColumn"
+        Me.AuthorDataGridViewTextBoxColumn.Width = 200
+        '
+        'YearPublishedDataGridViewTextBoxColumn
+        '
+        Me.YearPublishedDataGridViewTextBoxColumn.DataPropertyName = "YearPublished"
+        Me.YearPublishedDataGridViewTextBoxColumn.HeaderText = "YearPublished"
+        Me.YearPublishedDataGridViewTextBoxColumn.MinimumWidth = 10
+        Me.YearPublishedDataGridViewTextBoxColumn.Name = "YearPublishedDataGridViewTextBoxColumn"
+        Me.YearPublishedDataGridViewTextBoxColumn.Width = 200
+        '
+        'GenreDataGridViewTextBoxColumn
+        '
+        Me.GenreDataGridViewTextBoxColumn.DataPropertyName = "Genre"
+        Me.GenreDataGridViewTextBoxColumn.HeaderText = "Genre"
+        Me.GenreDataGridViewTextBoxColumn.MinimumWidth = 10
+        Me.GenreDataGridViewTextBoxColumn.Name = "GenreDataGridViewTextBoxColumn"
+        Me.GenreDataGridViewTextBoxColumn.Width = 200
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 585)
+        Me.Controls.Add(Me.dvgBooks)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dvgBooks, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LibraryDBDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BookssBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents dvgBooks As DataGridView
+    Friend WithEvents LibraryDBDataSet As LibraryDBDataSet
+    Friend WithEvents BookssBindingSource As BindingSource
+    Friend WithEvents BookssTableAdapter As LibraryDBDataSetTableAdapters.BookssTableAdapter
+    Friend WithEvents IDDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents TitleDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents AuthorDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents YearPublishedDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents GenreDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+End Class
---
+++ LIBRARYMANAGEMENTSYSTEMS/Form1.vb
@@ -0,0 +1,157 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    'variables used to connect to the DB
+    Dim sqlconn As SqlConnection
+    Dim sqlcmd As SqlCommand
+    Dim sqlRd As SqlDataReader
+    Dim sqlDt As New DataTable
+    Dim DtA As New SqlDataAdapter
+    Dim sqlQuery As String
+
+    'my mssql server and database name
+    Dim Server As String = "VENOMIZER"
+    Dim Database As String = "LibraryDB"
+
+    Private bitmap As Bitmap
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        updatetable()
+    End Sub
+
+    Private Sub updatetable()
+        Try
+            sqlconn = New SqlConnection()
+            sqlconn.ConnectionString = $"Server={Server};Database={Database};Integrated Security=True;"
+
+            sqlconn.Open()
+            sqlcmd = New SqlCommand("SELECT * FROM Books", sqlconn)
+            sqlRd = sqlcmd.ExecuteReader()
+
+            sqlDt.Clear()
+            sqlDt.Load(sqlRd)
+            sqlRd.Close()
+            sqlconn.Close()
+            dvgBooks.DataSource = sqlDt
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "MSSQL Connector", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            If sqlconn IsNot Nothing AndAlso sqlconn.State = ConnectionState.Open Then
+                sqlconn.Close()
+            End If
+        End Try
+    End Sub
+
+    'ADD button functionality
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            sqlconn = New SqlConnection()
+            sqlconn.ConnectionString = $"Server={Server};Database={Database};Integrated Security=True;"
+
+            sqlconn.Open()
+
+            sqlQuery = "INSERT INTO Books (Title, Author, YearPublished, Genre) VALUES (@Title, @Author, @YearPublished, @Genre)"
+
+            sqlcmd = New SqlCommand(sqlQuery, sqlconn)
+            sqlcmd.Parameters.AddWithValue("@Title", TextBox1.Text)
+            sqlcmd.Parameters.AddWithValue("@Author", TextBox2.Text)
+            sqlcmd.Parameters.AddWithValue("@YearPublished", TextBox3.Text)
+            sqlcmd.Parameters.AddWithValue("@Genre", TextBox4.Text)
+            sqlcmd.ExecuteNonQuery()
+            sqlconn.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "MSSQL Connector", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            If sqlconn IsNot Nothing AndAlso sqlconn.State = ConnectionState.Open Then
+                sqlconn.Close()
+            End If
+        End Try
+
+        updatetable()
+    End Sub
+
+
+
+    ' UPDATE button functionality
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            sqlconn = New SqlConnection()
+            sqlconn.ConnectionString = $"Server={Server};Database={Database};Integrated Security=True;"
+
+            sqlconn.Open()
+            'updates the row according to its title
+            sqlQuery = "UPDATE Books SET Author = @Author, Genre = @Genre, YearPublished = @YearPublished WHERE Title = @Title"
+
+            sqlcmd = New SqlCommand(sqlQuery, sqlconn)
+            sqlcmd.Parameters.AddWithValue("@Author", TextBox2.Text)
+            sqlcmd.Parameters.AddWithValue("@Genre", TextBox4.Text)
+            sqlcmd.Parameters.AddWithValue("@YearPublished", Integer.Parse(TextBox3.Text))
+            sqlcmd.Parameters.AddWithValue("@Title", TextBox1.Text)
+            sqlcmd.ExecuteNonQuery()
+            sqlconn.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "MSSQL Connector", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            If sqlconn IsNot Nothing AndAlso sqlconn.State = ConnectionState.Open Then
+                sqlconn.Close()
+            End If
+        End Try
+
+        updatetable()
+    End Sub
+    'DELETE button functionality
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            sqlconn = New SqlConnection()
+            sqlconn.ConnectionString = $"Server={Server};Database={Database};Integrated Security=True;"
+
+            sqlconn.Open()
+            'deletes the row wher a certain title exists
+            sqlQuery = "DELETE FROM Books WHERE Title = @Title"
+
+            sqlcmd = New SqlCommand(sqlQuery, sqlconn)
+            sqlcmd.Parameters.AddWithValue("@Author", TextBox2.Text)
+            sqlcmd.Parameters.AddWithValue("@Genre", TextBox4.Text)
+            sqlcmd.Parameters.AddWithValue("@YearPublished", Integer.Parse(TextBox3.Text))
+            sqlcmd.Parameters.AddWithValue("@Title", TextBox1.Text)
+            sqlcmd.ExecuteNonQuery()
+            sqlconn.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "MSSQL Connector", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            If sqlconn IsNot Nothing AndAlso sqlconn.State = ConnectionState.Open Then
+                sqlconn.Close()
+            End If
+        End Try
+
+        updatetable()
+    End Sub
+
+    'CLEAR button functionality
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            sqlconn = New SqlConnection()
+            sqlconn.ConnectionString = $"Server={Server};Database={Database};Integrated Security=True;"
+
+            sqlconn.Open()
+            'deletes all from table books
+            sqlQuery = "DELETE FROM Books"
+
+            sqlcmd = New SqlCommand(sqlQuery, sqlconn)
+            sqlcmd.ExecuteNonQuery()
+            sqlconn.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "MSSQL Connector", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            If sqlconn IsNot Nothing AndAlso sqlconn.State = ConnectionState.Open Then
+                sqlconn.Close()
+            End If
+        End Try
+
+        updatetable()
+    End Sub
+
+    Private Sub lblYearPublished_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
